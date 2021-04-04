1fc6e3620e1e59818974dc02a59b76001e2f5e98
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-04 14:12:53 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+/*function generateCardsArr() {
+  let arr = []
+  for (let index = 0; index < 8; index++) {
+    let len = Math.floor(Math.random() * lorem.length);
+    arr.push({
+      id: Number(cardCount++),
+      completed: Boolean(Math.floor(Math.random() * 2)),
+      text: lorem.slice(0, len)
+    })
+  }
+  return arr
+};*/
+
+export const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
