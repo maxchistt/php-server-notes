90fc7495d3af70904a5193e0533fe51f66e1d43a
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-05 05:31:37 +0300

    update

--- index.php
+++ index.php
@@ -9,16 +9,15 @@ $filepath = file_path($filename);
 
 include "./generate.php";
 
-if ($target == "ip") {
-    include "./getIp.php";
-    echo getIp();
+if (!$_POST) {
+    echo scandir(file_path(""));
 } else if ($target == "getData") {
     echo (readFileContent($filepath))
         ? (readFileContent($filepath))
         : (writeFileContent($filepath,  json_encode(generate())) ? readFileContent($filepath) : 'Ошибка при генерации данных');
 } else if ($target == "setData") {
     echo ($data)
-        ? (writeFileContent($filepath,$data) ? 'Данные в файл успешно занесены' : 'Ошибка при записи в файл')
+        ? (writeFileContent($filepath, $data) ? 'Данные в файл успешно занесены' : 'Ошибка при записи в файл')
         : ("Пустой набор слов");
 } else {
     echo "неверный запрос";
@@ -42,10 +41,4 @@ function writeFileContent($filepath, $text)
 function file_path($filename)
 {
     return "./data/" . "$filename";
-}
-
-function str_replace_once($search, $replace, $text)
-{
-    $pos = strpos($text, $search);
-    return $pos !== false ? substr_replace($text, $replace, $pos, strlen($search)) : $text;
 }
