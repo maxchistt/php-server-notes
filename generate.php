07fb562a90cf954ef6129fb111be01f9b918cfa9
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-11 11:02:03 +0300

    Update generate.php

--- generate.php
+++ generate.php
@@ -8,11 +8,13 @@ function generate()
     global $lorem_ipsum, $cardCount;
     $arr = [];
     for ($index = 0; $index < 8; $index++) {
-        $len = random_int(1, strlen($lorem_ipsum));
+        $lenText = random_int(1, strlen($lorem_ipsum));
+        $lenName = random_int(1, strlen($lorem_ipsum));
         $card = (object) [
             'id' => (int)($cardCount++),
+            'name' => (string)substr($lorem_ipsum, $lenName, ($lenName + 10) > strlen($lorem_ipsum) ? strlen($lorem_ipsum) : ($lenName + 10)),
             'completed' => (bool)(random_int(0, 1)),
-            'text' => (string)substr($lorem_ipsum, 0, $len)
+            'text' => (string)substr($lorem_ipsum, 0, $lenText)
         ];
         array_push($arr, $card);
     };
