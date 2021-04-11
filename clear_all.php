83716214214fff0e1f340d224a0cfccdd2cd55ca
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-11 08:13:54 +0300

    mr

--- clear_all.php
+++ clear_all.php
@@ -13,7 +13,7 @@ function clearAll()
     }
     if (!$str) $str = "none";
     return "
-    <div class='container' style='white-space: normal;word-wrap: break-word;'>
+    <div class='container mt-3' style='white-space: normal;word-wrap: break-word;'>
     <br/>
     Deleted files:
     <br/><hr/>
