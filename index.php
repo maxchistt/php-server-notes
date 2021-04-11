4dee9e0b332602b3a4d45c7a623b8799c576138a
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-11 07:37:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,11 +18,8 @@ if (!$_POST) {
     $fileslist = "$mark " . json_encode(scandir(file_path("")));
     include "./std_resp.php";
     include "./clear_all.php";
-    if ($req == "clear-all") {
-        echo clearAll();
-    } else {
-        echo makeHtmlResp($met, $req, $fileslist);
-    }
+    if ($req["clear-all"]) echo clearAll();
+    echo makeHtmlResp($met, $req, $fileslist);
 } else if ($user && $target) {
     if ($target == "getData") {
         echo (readFileContent($filepath))
