d99244d701f149df3e0a9905b002ddfa143f37d2
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-15 22:59:28 +0300

    Update generate.php

--- generate.php
+++ generate.php
@@ -2,7 +2,17 @@
 include "./lorem.php";
 
 $cardCount = 1;
-$colors = ["#F38181", "#FCE38A", "#EAFFD0"];
+$colors = [
+    "#f8f9fa",
+    "#F38181",
+    "#FCE38A",
+    "#EAFFD0",
+    "#F9FFEA",
+    "#8785A2",
+    "#DBE2EF",
+    "#D4A5A5",
+    "#6EF3D6"
+];
 
 function generate()
 {
@@ -14,7 +24,7 @@ function generate()
         $card = (object) [
             'id' => (int)($cardCount++),
             'name' => (string)substr($lorem_ipsum, $lenName, ($lenName + 30) > strlen($lorem_ipsum) ? ($lenName + 30) - strlen($lorem_ipsum) : (random_int(10, 30))),
-            'color' => (string)($colors[random_int(0, 2)]),
+            'color' => (string)($colors[random_int(0, 8)]),
             'text' => (string)substr($lorem_ipsum, 0, $lenText),
             'completed' => (bool)(random_int(0, 1)),
         ];
